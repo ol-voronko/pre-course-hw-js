40d9680923461000e611044f8bd2223e8f2fd601
ol-voronko <voronyatco@gmail.com>
2023-12-26 19:31:25 +0200

    numbers.map(row=>console.log(row.filter(number=>!(number % 2))))

---
+++ lesson-03/07.js
@@ -0,0 +1,11 @@
+const numbers = [
+  [1, 2, 3, 4, 5],
+  [11, 20, 33, 40, 55],
+  [111, 200, 333, 400, 555],
+];
+ 
+numbers.map(row => console.log(
+    row.filter
+        (number =>!(number % 2)))
+); 
+ 
